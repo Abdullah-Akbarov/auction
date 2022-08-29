b13a332edcc5acf3c0be92b6bf17d086e7c3d64e
Abdulloh <abdullah.akbarov.com>
2022-08-30 02:59:53 +0500

    Lot filter changed and another filter for lot added

--- src/main/java/component/LotFilter.java
+++ src/main/java/component/LotFilter.java
@@ -1,53 +1,37 @@
 package component;
 
+
 import com.google.gson.Gson;
-import dao.UserDao;
-import dao.impl.UserDaoImpl;
+import dao.LotDao;
+import dao.impl.LotDaoImpl;
+import model.Lot;
 import model.Message;
-import model.User;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Base64;
 import java.util.Optional;
 
-@WebFilter("/add-lot")
+@WebFilter("/close-lot")
 public class LotFilter implements Filter {
     private final Gson gson = new Gson();
-    private final UserDao userDao = UserDaoImpl.getUserDao();
-
+    private final LotDao lotDao = LotDaoImpl.getLotDao();
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletResponse.setContentType("application/json");
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        String uri = request.getRequestURI();
-        if (uri.startsWith("/login") || uri.startsWith("/view") || uri.startsWith("/bid")) {
-            filterChain.doFilter(servletRequest, servletResponse);
-        } else {
-            String key = request.getHeader("key");
-            if (key == null || key.isEmpty() || key.isBlank()) {
-                response.getWriter().print(gson.toJson(new Message(401, "Unauthorized", null)));
+        String id = request.getParameter("id");
+        Optional<Lot> byId = lotDao.getById(Integer.parseInt(id));
+        if (byId.isPresent()){
+            if (!byId.get().isActive()){
+                response.getWriter().print(gson.toJson(new Message(410, "Lot Has been closed", null)));
                 response.getWriter().close();
-            } else {
-                String decode = new String(Base64.getDecoder().decode(key.getBytes()));
-                String[] split = decode.split("&");
-                if (split.length == 2) {
-                    Optional<User> userOptional = userDao.findByUsernameAndPassword(split[0], split[1]);
-                    if (userOptional.isPresent() && userOptional.get().isAdmin()) {
-                        HttpSession session = request.getSession();
-                        session.setAttribute("user", userOptional.get());
-                        filterChain.doFilter(servletRequest, servletResponse);
-                    } else {
-                        response.getWriter().print(gson.toJson(new Message(401, "Admin role required", null)));
-                        response.getWriter().close();
-                    }
-                }
             }
+        } else {
+            response.getWriter().print(gson.toJson(new Message(404, "Not Found", null)));
+            response.getWriter().close();
         }
     }
 }
